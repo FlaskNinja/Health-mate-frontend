d136915d69c5c9dd3524fef03ad3842de09f8fc5
FlaskNinja <usmanmariam43@gmail.com>
2025-10-01 17:22:17 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Login from "./pages/login";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Dashboard from "./pages/Dashboard";
 import Profile from "./pages/Profile";
-import ChatBot from "./pages/Chatbot";
 
 
 function App() {
@@ -17,7 +16,6 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/chatbot" element={<Chatbot />} />
       </Routes>
     </BrowserRouter>
   )
